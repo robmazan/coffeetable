a5b566f12f973ab7cb361b085277a209c3e19d4d
Mazán Róbert <robmazan@Mazans-MacBook-Air.local>
2020-07-26 19:46:43 +0200

    Basic file upload form

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form method="POST" encType="multipart/form-data" action="/upload">
+      <input name="file" type="file" multiple />
+      <button type="submit">Send</button>
+    </form>
   );
 }
 
